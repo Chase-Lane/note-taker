9e68417cab78584df3fbc10e35bb1a1943560f33
Chase-Lane <32445614+Chase-Lane@users.noreply.github.com>
2023-01-05 23:01:04 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const fs = require('fs');
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const path = require('path');
 const app = express();
 const dbinfo = require('./db/db.json');
